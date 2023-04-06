5c2056de24beb00fc022399dd9055eec09d367cc
SaranKaewchuay <saranyeen@gmail.com>
2023-04-06 23:19:49 +0700

    update

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ app.get("/getsolr/:keyword", async (req, res) => {
   const keyword = req.params.keyword;
   try {
     const data = await sendRequestGetJson(
-      `http://localhost:8983/solr/thai_research/select?facet.field=ProjectYearSubmit&facet.field=OECD1&facet.field=SubmitDepProvinceTH&facet=true&indent=true&q.op=OR?indent=true&q.op=OR&q=ProjectNameTH%3A` + keyword + `*&rows=180&start=0`
+      `http://localhost:8983/solr/thai_research/select?facet.field=ProjectYearSubmit&facet.field=OECD1&facet.field=SubmitDepProvinceTH&facet=true&indent=true&q.op=OR?indent=true&q.op=OR&q=ProjectNameTH%3A` + keyword +`&q=ProjectKeyword%3A` + keyword +`&q=_text_%3A` + keyword +`&rows=180&start=0`
     );
-    
+
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({
@@ -33,7 +33,7 @@ app.get("/getByOecd/:oecd", async (req, res) => {
   const oecd = req.params.oecd
   try {
     const data = await sendRequestGetJson(
-      `http://localhost:8983/solr/thai_research/select?indent=true&q.op=OR&q=OECD1:`+ oecd + `*&rows=180&start=0`
+      `http://localhost:8983/solr/thai_research/select?indent=true&q.op=OR&q=OECD1:`+ oecd + `&rows=180&start=0`
         
     );
 
@@ -50,7 +50,7 @@ app.get("/getbyid/:id", async (req, res) => {
   const id = req.params.id;
   try {
     const data = await sendRequestGetJson(
-      `http://localhost:8983/solr/thai_research/select?indent=true&q.op=OR&q=id%3A` + id + `*&rows=180&start=0`
+      `http://localhost:8983/solr/thai_research/select?indent=true&q.op=OR&q=id%3A` + id + `&rows=180&start=0`
         
     );
 
@@ -66,7 +66,7 @@ app.get("/getByYear/:year", async (req, res) => {
   const year= req.params.year;
   try {
     const data = await sendRequestGetJson(
-      `http://localhost:8983/solr/thai_research/select?indent=true&q.op=AND&q=ProjectYearSubmit%3A` + year + `*&rows=180&start=0`
+      `http://localhost:8983/solr/thai_research/select?indent=true&q.op=AND&q=ProjectYearSubmit%3A` + year + `&rows=180&start=0`
         
     );
 
@@ -84,7 +84,7 @@ app.get("/getDataByFact/:key/:value", async (req, res) => {
   const value = req.params.value;
   try {
     const data = await sendRequestGetJson(
-      `http://localhost:8983/solr/thai_research/select?indent=true&q.op=AND&q=`+ key +`%3A` + value + `*&rows=180&start=0`
+      `http://localhost:8983/solr/thai_research/select?indent=true&q.op=AND&q=`+ key +`%3A` + value + `&rows=180&start=0`
         
     );
 
@@ -103,9 +103,7 @@ app.get("/getDataBoolean/:year/:province/:oecd", async (req, res) => {
   const oecd = req.params.oecd;
   try {
     const data = await sendRequestGetJson(
-      // `http://localhost:8983/solr/thai_research/select?facet=true&fq=OECD1%3A`+*+`&fq=ProjectYearSubmit%3A`+*+`&fq=SubmitDepProvinceTH%3A`+*+`&indent=true&q.op=AND&q=*%3A*`
-      `http://localhost:8983/solr/thai_research/select?facet.field=ProjectYearSubmit&facet.field=OECD1&facet.field=SubmitDepProvinceTH&facet=true&fq=ProjectYearSubmit%3A`+ year +`&fq=SubmitDepProvinceTH%3A`+ province +`&indent=true&q.op=AND&q=OECD1%3A`+ oecd +`*&rows=180&start=0`
-        
+      `http://localhost:8983/solr/thai_research/select?facet.field=ProjectYearSubmit&facet.field=OECD1&facet.field=SubmitDepProvinceTH&facet=true&fq=ProjectYearSubmit%3A`+ year +`&fq=SubmitDepProvinceTH%3A`+ province +`&indent=true&q.op=AND&q=OECD1%3A`+ oecd +`&rows=180&start=0` 
     );
 
     res.status(200).json(data);
